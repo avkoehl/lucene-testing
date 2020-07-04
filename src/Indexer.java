ea5dac9a14cb9ee3a79f08993348f1d124fb0390
Arthur Koehl <avkoehl@ucdavis.edu>
2020-07-04 16:53:01 -0700

    handles the csv fields if there is a header

--- src/Indexer.java
+++ src/Indexer.java
@@ -1,8 +1,18 @@
 package com.arthurkoehl.lucene;
 
+import java.io.Reader;
 import java.io.IOException;
 import java.io.StringReader;
 import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
@@ -20,6 +30,16 @@ public class Indexer {
     public Indexer() {
     }
 
+    public List<String[]> readCSV(String fname) throws Exception {
+	Reader reader = Files.newBufferedReader(Paths.get(fname));
+	CSVReader csvReader = new CSVReader(reader);
+	List<String[]> list = new ArrayList<>();
+	list = csvReader.readAll();
+	reader.close();
+	csvReader.close();
+	return list;
+    }
+
     public static void main(String[] args) throws IOException {
 	Indexer indexer = new Indexer();
 	System.out.println("Running Main");
@@ -31,23 +51,26 @@ public class Indexer {
 	writer.deleteAll();
 	writer.commit();
 
-	String[] texts = {
-	    "Agrarian practices in the west",
-	    "western climates",
-	    "environmental changes over time",
-	    "settlement and colonial history",
-	    "urban identity and landscapes",
-	    "self isolation in hermits",
-	    "practices and beliefs of the poor",
-	    "sailors and their lives",
-	    "small villages in alsace",
-	    "urban organization and communities",
-	};
-
-	for (int i = 0; i < texts.length; i++) {
-	    Document doc = new Document();
-	    doc.add(new TextField("title", texts[i], Field.Store.YES));
-	    writer.addDocument(doc);
+	List<String[]> data = new LinkedList<String[]>();
+	List<String> fields = new LinkedList<String>();
+	try {
+	    data = indexer.readCSV("data/deniro.csv");
+	    for (int i = 0; i < data.get(0).length; i++)
+	    {
+		fields.add(data.get(0)[i]);
+	    }
+	    for (int i = 1; i < data.size(); i++)
+	    {
+	        Document doc = new Document();
+		for (int j = 0; j < data.get(i).length; j++) {
+		    doc.add(new TextField(fields.get(j), data.get(i)[j], 
+				Field.Store.YES));
+		}
+		writer.addDocument(doc);
+	    }
+	}
+	catch(Exception e) {
+	    e.printStackTrace();
 	}
 
 	writer.close();
