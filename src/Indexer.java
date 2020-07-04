171509d67a2ce354d78ed78088fe1bc099bfe1e9
Arthur Koehl <avkoehl@ucdavis.edu>
2020-07-03 23:37:14 -0700

    getting there

--- src/Indexer.java
+++ src/Indexer.java
@@ -1,9 +1,10 @@
 // adapted from http://web.cs.ucla.edu/classes/winter15/cs144/projects/lucene/index.html
-//package lucene.demo.search;
+package com.arthurkoehl.lucene;
 
 import java.io.IOException;
 import java.io.StringReader;
-import java.io.File;
+import java.nio.file.Paths;
+
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
@@ -18,38 +19,40 @@ import org.apache.lucene.util.Version;
 
 public class Indexer {
 
-    /** Creates a new instance of Indexer */
-    public Indexer() {
-    }
+    public void indexReference(String line, IndexWriter writer) throws IOException {
 
-    private IndexWriter indexWriter = null;
-
-    public IndexWriter getIndexWriter(boolean create) throws IOException {
-	if (indexWriter == null) {
-	    Directory indexDir = FSDirectory.open(new File("index-directory"));
-	    IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_4_10_2, new StandardAnalyzer());
-	    indexWriter = new IndexWriter(indexDir, config);
-	}
-	return indexWriter;
-    }
-
-    public void closeIndexWriter() throws IOException {
-	if (indexWriter != null) {
-	    indexWriter.close();
-	}
-    }
-
-    public void indexHotel(Hotel hotel) throws IOException {
-
-	System.out.println("Indexing hotel: " + hotel);
-	IndexWriter writer = getIndexWriter(false);
+	System.out.println("Indexing: " + line);
 	Document doc = new Document();
-	doc.add(new StringField("id", hotel.getId(), Field.Store.YES));
-	doc.add(new StringField("name", hotel.getName(), Field.Store.YES));
-	doc.add(new StringField("city", hotel.getCity(), Field.Store.YES));
-	String fullSearchableText = hotel.getName() + " " + hotel.getCity() + " " + hotel.getDescription();
-	doc.add(new TextField("content", fullSearchableText, Field.Store.NO));
+	doc.add(new StringField("title", line, Field.Store.YES));
 	writer.addDocument(doc);
+    }
+
+    public void buildIndexes() throws IOException {
+	System.out.println("Building Index");
+	Directory indexDir = FSDirectory.open(Paths.get("index"));
+	IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());
+	IndexWriter writer = new IndexWriter(indexDir, config);
+
+	String line = "Agrarian practices in the American West";
+	try {
+	    this.indexReference(line, writer);
+	}
+	catch(IOException e) {
+	    e.printStackTrace();
+	}
+
+	writer.close();
+    }
+
+    public static void main(String[] args) {
+	Indexer indexer = new Indexer();
+	System.out.println("Running Main");
+	try {
+	    indexer.buildIndexes();
+	}
+	catch(IOException e) {
+	    e.printStackTrace();
+	}
     }
 
 }
